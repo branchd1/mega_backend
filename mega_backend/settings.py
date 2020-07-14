b5a3585e0555daf328ea0a411d32621e5bcfdaf0
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-07-14 20:21:24 +0100

    rest framework token auth

--- mega_backend/settings.py
+++ mega_backend/settings.py
@@ -126,8 +126,8 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication'
+        'rest_framework.authentication.SessionAuthentication'
     ]
 }
