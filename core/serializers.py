564e996f2576d9a54093373d002709b46964c6ea
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-07-15 10:43:47 +0100

    serializers and viewset

--- core/serializers.py
+++ core/serializers.py
@@ -1,3 +1,33 @@
 from rest_framework import serializers
 
+from .models import Profile, Feature, Database, Community
+
 # Serializers
+
+class ProfileSerializer(serializers.Serializer):
+	''' Serialize profile '''
+	class Meta:
+		model = Profile
+		fields = '__all__'
+		read_only_fields = ['id']
+		
+class FeatureSerializer(serializers.Serializer):
+	''' Serialize feature '''
+	class Meta:
+		model = Feature
+		fields = '__all__'
+		read_only_fields = ['id']
+		
+class DatabaseSerializer(serializers.Serializer):
+	''' Serialize database '''
+	class Meta:
+		model = Database
+		fields = '__all__'
+		read_only_fields = ['id']
+		
+class CommunitySerializer(serializers.Serializer):
+	''' Serialize community '''
+	class Meta:
+		model = Community
+		fields = '__all__'
+		read_only_fields = ['id']
--- core/views.py
+++ core/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import Profile, Feature, Database, Community
+
+from .serializers import ProfileSerializer, FeatureSerializer, DatabaseSerializer, CommunitySerializer
+
+# Views here
+
+class ProfileViewSet(viewsets.ModelViewSet):
+	''' profile view set '''
+	serializer_class = ProfileSerializer
+	permission_classes = []
+	
+	def get_queryset(self):
+		return self.request.user.profile.all()
+		
+class FeatureViewSet(viewsets.ModelViewSet):
+	''' feature view set '''
+	serializer_class = FeatureSerializer
+	permission_classes = []
+		
+class DatabaseViewSet(viewsets.ModelViewSet):
+	''' database view set '''
+	serializer_class = DatabaseSerializer
+	permission_classes = []
+		
+class CommunityViewSet(viewsets.ModelViewSet):
+	''' community view set '''
+	serializer_class = CommunitySerializer
+	permission_classes = []
+	
