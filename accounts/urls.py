aaa53ace39246d5dbfc3edcc016f7333f9c2b976
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-08-27 05:33:16 +0100

    reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 
 from django.contrib.auth import views as auth_views
 
@@ -10,7 +10,8 @@ urlpatterns = [
     path('password/reset/confirm/<uidb64>/<token>',
          auth_views.PasswordResetConfirmView.as_view(
              template_name='accounts/registration/my_password_reset_confirm.html',
-             form_class=MySetPasswordForm
+             success_url=reverse_lazy('accounts:password_reset_complete'),
+             form_class=MySetPasswordForm,
          ),
          name='password_reset_confirm'),
     path('password/reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
