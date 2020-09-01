a7deaafc02f4a8be35703acf14427103f0c6c7ec
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-09-01 17:18:01 +0100

    multipart form

--- core/serializers.py
+++ core/serializers.py
@@ -7,7 +7,6 @@ from . import models as my_models
 
 class FeatureSerializer(serializers.ModelSerializer):
     """ Serialize feature """
-
     class Meta:
         model = my_models.Feature
         fields = '__all__'
@@ -26,24 +25,7 @@ class CommunitySerializer(serializers.ModelSerializer):
 
 class SimpleStoreSerializer(serializers.ModelSerializer):
     """ Serialize simple store """
-
     class Meta:
         model = my_models.SimpleStore
         fields = ['id', 'key', 'value']
         read_only_fields = ['id']
-
-# class ListStoreSerializer(serializers.ModelSerializer):
-# 	""" Serialize list store """
-# 	class Meta:
-# 		model = my_models.ListStore
-# 		fields = '__all__'
-# 		read_only_fields = ['id']
-# 		exclude = ['content_type', 'object_id']
-#
-# class MapStoreSerializer(serializers.ModelSerializer):
-# 	""" Serialize map store """
-# 	class Meta:
-# 		model = my_models.MapStore
-# 		fields = '__all__'
-# 		read_only_fields = ['id']
-# 		exclude = ['content_type', 'object_id']
--- core/templatetags/core/form.py
+++ core/templatetags/core/form.py
@@ -4,7 +4,8 @@ register = template.Library()
 
 
 @register.inclusion_tag('core/form_template.html', takes_context=True)
-def form(context):
+def form(context, is_multipart=False):
     return {
-        'form': context['form']
+        'form': context['form'],
+        'is_multipart': is_multipart
     }
