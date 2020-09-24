0360fbfdcbc17449ee237af3492c6e097981f2a6
Arnold Umakhihe <arnoldumakhihe@Arnolds-MacBook-Pro.local>
2020-09-25 00:18:15 +0100

    aws storage

--- my_storages/storage_backends.py
+++ my_storages/storage_backends.py
@@ -7,7 +7,7 @@ class StaticStorage(S3Boto3Storage):
     default_acl = 'public-read'
 
 
-class MediaStorage(S3Boto3Storage):
+class PublicMediaStorage(S3Boto3Storage):
     location = 'media'
     default_acl = 'public-read'
     file_overwrite = False
